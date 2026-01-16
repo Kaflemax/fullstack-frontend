783d949ff92b4b22e486c32138f6226bb98369f5
Shisir Kafle <shisir.kafle@gmail.com>
2026-01-16 12:56:04 +0545

    added bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddUser from "./users/AddUser";
-import EditUser from "./users/EditUser";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/adduser" element={<AddUser/>}/>
-        <Route exact path="/edituser/:id" element={<EditUser/>}/>
-       
-      </Routes>
-    </Router>
+    <div className="App">
+      <Navbar/>
+    </div>
   );
 }
 
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,34 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Full Stack Application
-        </Link>
+    <div>
+<nav classname="navbar navbar-expand-lg bg-body-light">
+  <div classname="container-fluid">
+    <a classname="navbar-brand" href="#">
+      Navbar
+      </a>
+    <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span classname="navbar-toggler-icon"></span>
+    </button>
+    
+  </div>
+</nav>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className="ms-auto">
-            <Link className="btn btn-outline-light" to="/adduser">
-              Add User
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+    </div>
+  )
 }
